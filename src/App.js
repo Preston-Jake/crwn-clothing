e0e75e4c43470fa04b20e5b3c2a4092de308420c
JakePreston <prestonjak@outlook.com>
2022-01-09 18:43:22 -0600

    added directory and menu-items components and added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
-import HomePage from './homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/home/homepage.component";
+import { Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <HomePage />
     </div>
   );
 }
