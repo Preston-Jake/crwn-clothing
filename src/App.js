55a3e0e692601d36225c4e7b6197dcd02719f1eb
JakePreston <prestonjak@outlook.com>
2022-01-11 10:41:48 -0600

    add app header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
+import { Header } from './components/header/header.component';
 
-import HomePage from "./pages/home/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import HomePage from './pages/home/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/shop' element={<ShopPage/>} />
+        <Route path='/' element={<HomePage />} />
+        <Route path='/shop' element={<ShopPage />} />
       </Routes>
     </div>
   );
